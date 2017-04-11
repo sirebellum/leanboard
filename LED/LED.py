cb474431fa4702879e0a8aaae2a533bbb071424f
Leanboard Pi <multimedia@utsa.edu>
2017-04-11 17:50:51 -0500

    Implemented bitwise buffer operations

--- LED/LED.py
+++ LED/LED.py
@@ -10,25 +10,42 @@ GPIO.setup(latch, GPIO.OUT)
 spi = spidev.SpiDev()
 spi.open(0, 1)
 
-buffer_1 = list(0x00 for x in range(0, 36))
-for x in range(18, 36):
-    buffer_1[x] = 0xFF
-buffer_2 = list(0xFF for x in range(0, 36))
-for x in range(18, 36):
-    buffer_2[x] = 0x00
+buffer = list(0x00 for x in range(0, 36))
 
 try:
     while True:
+        
+        buffer[0]|= 0xFF
+        buffer[1]|= 0xF0
+        buffer[18]|= 0xFF
+        buffer[19]|= 0xF0
+        
+        buffer[16]&= 0xF0
+        buffer[17]&= 0x00
+        buffer[34]&= 0xF0
+        buffer[35]&= 0x00
+        
         for i in range(0, 36):
-            spi.xfer2([buffer_1[i]])
+            spi.xfer2([buffer[i]])
         
         GPIO.output(latch, GPIO.HIGH)
         GPIO.output(latch, GPIO.LOW)    
         
         time.sleep(1)
         
+        buffer[16]|= 0x0F
+        buffer[17]|= 0xFF
+        buffer[34]|= 0x0F
+        buffer[35]|= 0xFF
+        
+        buffer[0]&= 0x00
+        buffer[1]&= 0x0F
+        buffer[18]&= 0x00
+        buffer[19]&= 0x0F
+        
+        
         for i in range(0, 36):
-            spi.xfer2([buffer_2[i]])
+            spi.xfer2([buffer[i]])
             
         GPIO.output(latch, GPIO.HIGH)
         GPIO.output(latch, GPIO.LOW)
