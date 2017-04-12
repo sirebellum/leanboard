d2687128d22a407eedbe643b54e47e9caa1f2319
Leanboard Pi <multimedia@utsa.edu>
2017-04-11 19:17:56 -0500

    Added LED Toggle

---
+++ LED_functions.py
@@ -0,0 +1,52 @@
+import spidev
+
+def LED_On(LED, buffer):
+    
+    if (LED % 2) == 0: #even
+        i = LED*3/2
+        buffer[i]|= 0xFF
+        buffer[i+1]|= 0xF0
+        return i
+        
+    elif (LED % 2) != 0: #odd
+        i = ((LED-1) * 3/2) + 1
+        buffer[i]|= 0x0F
+        buffer[i+1]|= 0xFF
+        return i
+
+
+        
+def LED_Off(LED, buffer):
+    
+    if (LED % 2) == 0: #even
+        i = LED*3/2
+        buffer[i]&= 0x00
+        buffer[i+1]&= 0x0F
+        return i
+        
+    elif (LED % 2) != 0: #odd
+        i = ((LED-1) * 3/2) + 1
+        buffer[i]&= 0xF0
+        buffer[i+1]&= 0x00
+        return i
+    
+    
+    
+def LED_Toggle(LED, buffer):
+    
+    if (LED % 2) == 0: #even
+        i = LED*3/2
+        buffer[i]^= 0xFF
+        buffer[i+1]^= 0xF0
+        return i
+        
+    elif (LED % 2) != 0: #odd
+        i = ((LED-1) * 3/2) + 1
+        buffer[i]^= 0x0F
+        buffer[i+1]^= 0xFF
+        return i
+
+        
+def LED_write(buffer):
+    for i in range(0, 36):
+        spi.xfer2([buffer[i]])
